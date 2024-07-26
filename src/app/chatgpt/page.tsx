c05135e3676a802c51e0d49c39353818d7e72c42
“chutikan” <chutikanKK88@gmail.com>
2024-07-26 12:39:41 +0700

    first commit

---
+++ src/app/chatgpt/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ChatGPT from '../components/ChatGPT'
+
+export default function page() {
+  return (
+    <div>
+      <ChatGPT/>
+    </div>
+  )
+}
--- src/app/components/Navber.tsx
+++ src/app/components/Navber.tsx
@@ -29,7 +29,7 @@ export default function Navber() {
                 <a href="/evaluation">การประเมินนโยบายสาธารณะ</a>
               </li>
               <li className="text-stone-600 hover:text-blue-600 hover:font-bold font-medium mx-4 my-1">
-                <a href="#">ChatGPT</a>
+                <a href="/chatgpt">ChatGPT</a>
               </li>
               <li className="text-stone-600 hover:text-blue-600 hover:font-bold font-medium mx-4 my-1">
                 <a href="#">ทีมงาน</a>
